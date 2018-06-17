463f66b0078bf1dde62086ce72efb5711734978c
Joshua Chesney <joshchesney36@gmail.com>
2018-06-17 16:25:19 -0500

    Initial framework for program started

--- BudgetMaker/Form1.Designer.cs
+++ BudgetMaker/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BudgetMaker
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblAppTitle = new System.Windows.Forms.Label();
+            this.lblExpenses = new System.Windows.Forms.Label();
+            this.lblIncome = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.signInToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.signInToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.signOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.financeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addIncomeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addExpenseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1072, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(363, 238);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblAppTitle
+            // 
+            this.lblAppTitle.AutoSize = true;
+            this.lblAppTitle.Font = new System.Drawing.Font("Bernard MT Condensed", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAppTitle.Location = new System.Drawing.Point(12, 59);
+            this.lblAppTitle.Name = "lblAppTitle";
+            this.lblAppTitle.Size = new System.Drawing.Size(388, 57);
+            this.lblAppTitle.TabIndex = 1;
+            this.lblAppTitle.Text = "Budget Master 2018";
+            // 
+            // lblExpenses
+            // 
+            this.lblExpenses.AutoSize = true;
+            this.lblExpenses.Location = new System.Drawing.Point(19, 167);
+            this.lblExpenses.Name = "lblExpenses";
+            this.lblExpenses.Size = new System.Drawing.Size(102, 13);
+            this.lblExpenses.TabIndex = 2;
+            this.lblExpenses.Text = "Average Expenses: ";
+            // 
+            // lblIncome
+            // 
+            this.lblIncome.AutoSize = true;
+            this.lblIncome.Location = new System.Drawing.Point(19, 131);
+            this.lblIncome.Name = "lblIncome";
+            this.lblIncome.Size = new System.Drawing.Size(88, 13);
+            this.lblIncome.TabIndex = 3;
+            this.lblIncome.Text = "Average Income:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.signInToolStripMenuItem,
+            this.financeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1447, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // signInToolStripMenuItem
+            // 
+            this.signInToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.signInToolStripMenuItem1,
+            this.signOutToolStripMenuItem});
+            this.signInToolStripMenuItem.Name = "signInToolStripMenuItem";
+            this.signInToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.signInToolStripMenuItem.Text = "Users";
+            // 
+            // signInToolStripMenuItem1
+            // 
+            this.signInToolStripMenuItem1.Name = "signInToolStripMenuItem1";
+            this.signInToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.signInToolStripMenuItem1.Text = "Sign In";
+            // 
+            // signOutToolStripMenuItem
+            // 
+            this.signOutToolStripMenuItem.Name = "signOutToolStripMenuItem";
+            this.signOutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.signOutToolStripMenuItem.Text = "Sign Out";
+            // 
+            // financeToolStripMenuItem
+            // 
+            this.financeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addIncomeToolStripMenuItem,
+            this.addExpenseToolStripMenuItem});
+            this.financeToolStripMenuItem.Name = "financeToolStripMenuItem";
+            this.financeToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.financeToolStripMenuItem.Text = "Finance";
+            // 
+            // addIncomeToolStripMenuItem
+            // 
+            this.addIncomeToolStripMenuItem.Name = "addIncomeToolStripMenuItem";
+            this.addIncomeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addIncomeToolStripMenuItem.Text = "Add income";
+            this.addIncomeToolStripMenuItem.Click += new System.EventHandler(this.addIncomeToolStripMenuItem_Click);
+            // 
+            // addExpenseToolStripMenuItem
+            // 
+            this.addExpenseToolStripMenuItem.Name = "addExpenseToolStripMenuItem";
+            this.addExpenseToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addExpenseToolStripMenuItem.Text = "Add expense";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1447, 970);
+            this.Controls.Add(this.lblIncome);
+            this.Controls.Add(this.lblExpenses);
+            this.Controls.Add(this.lblAppTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Budget Master";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblAppTitle;
+        private System.Windows.Forms.Label lblExpenses;
+        private System.Windows.Forms.Label lblIncome;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem signInToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem signInToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem signOutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem financeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addIncomeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addExpenseToolStripMenuItem;
     }
 }
 
---
+++ BudgetMaker/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetMaker
+{
+    class User
+    {
+        List<double> incomes = new List<double>();
+        List<double> expenses = new List<double>();
+    }
+}
